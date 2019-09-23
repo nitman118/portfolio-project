77b0da8eed57e31fdf46e64b509e32a71eeeab8f
Nitish Singh <nitman118@gmail.com>
2019-09-23 20:11:27 +0200

    set up blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+# title
+# publication date
+# body
+# image
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
+
+
+# create a blog model
+
+# add blog app to settings
+# create a migration
+# migrate
+# add to admin
