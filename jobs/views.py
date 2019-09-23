b23b92cd642830deea9acd224ba452fa67577387
Nitish Singh <nitman118@gmail.com>
2019-09-23 22:33:54 +0200

    setup homepage

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+#access to models
+from .models import Job
 # Create your views here.
+
+def home(request):
+    '''
+    homepage view
+    '''
+    jobs = Job.objects.all()
+    return render(request, 'jobs/home.html', {'jobs':jobs})
