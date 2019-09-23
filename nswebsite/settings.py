275b9efb4f3a725a5364b665efba42267d6bc4e6
Nitish Singh <nitman118@gmail.com>
2019-09-23 17:00:45 +0200

    added media root and media url

--- nswebsite/settings.py
+++ nswebsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my apps
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#FOR MEDIA - self made
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+# url if someone wants to access media
+MEDIA_URL = '/media/'
